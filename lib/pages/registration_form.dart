f9f1ffe3623ccb58acaebd71b797e072791d4412
mushtak <mushtakkhan9@gmail.com>
2022-09-27 13:09:50 +0530

    dropdown list fixed

--- lib/pages/registration_form.dart
+++ lib/pages/registration_form.dart
@@ -2,6 +2,7 @@ import 'package:dropdown_textfield/dropdown_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:greeting_app/pages/register.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:greeting_app/static_data.dart';
 import 'package:greeting_app/user_details.dart';
 
 class RegistrationForm extends StatefulWidget {
@@ -16,181 +17,17 @@ class _RegistrationFormState extends State<RegistrationForm> {
   FocusNode textFieldFocusNode = FocusNode();
   final _formKey = GlobalKey<FormState>();
 
-  final dept = [
-    {'name': 'hello1', 'value': 'value1'},
-    {'name': 'hello2', 'value': 'value2'},
-    {'name': 'hello3', 'value': 'value3'},
-  ];
+  final dept = ['1', '2', '3'];
 
-  final dept_name = const [
-    DropDownValueModel(name: 'Select Dept', value: "Select Dept"),
-    DropDownValueModel(name: 'Department of Technology', value: "btech"),
-    DropDownValueModel(
-      name: 'Department of Science',
-      value: "science",
-    ),
-    DropDownValueModel(
-        name: 'Department of Mass Communication & Journalism', value: "mass"),
-    DropDownValueModel(
-      name: 'Polytechnic',
-      value: "poly",
-    ),
-    DropDownValueModel(
-        name: 'Department of Education & Training', value: "edu"),
-    DropDownValueModel(
-        name: 'Department of Commerce & Business Management',
-        value: "comm & mang"),
-    DropDownValueModel(
-        name: 'Department of Arts & Social Science', value: "arts"),
-    DropDownValueModel(name: 'Department of Arabic', value: "arabic"),
-    DropDownValueModel(name: 'Department of English', value: "eng"),
-    DropDownValueModel(name: 'Department of Hindi', value: "hindi"),
-    DropDownValueModel(
-        name: 'Department of Persian and Central Asian Studies',
-        value: "persian"),
-    DropDownValueModel(
-        name: 'Department of Translation Studies', value: "trans"),
-    DropDownValueModel(name: 'Department of Urdu', value: "urdu"),
-    DropDownValueModel(name: 'Department of Economics', value: "eco"),
-    DropDownValueModel(name: 'Department of History', value: "his"),
-    DropDownValueModel(
-        name: 'Department of Political Science', value: "poli sci"),
-    DropDownValueModel(
-        name: 'Department of Public Administration', value: "pub admin"),
-    DropDownValueModel(name: 'Department of Social Work', value: "social"),
-    DropDownValueModel(name: 'Department of Sociology', value: "sociology"),
-    DropDownValueModel(
-        name: 'Department of Women Education', value: "women edu"),
-    DropDownValueModel(
-        name: 'Department of Islamic Studies', value: "islamic stu"),
-  ];
-  final dict = const {
-    'btech': [
-      DropDownValueModel(name: 'B.Tech Computer Science', value: "value1"),
-      DropDownValueModel(name: 'M.Tech Computer Science', value: "value2"),
-      DropDownValueModel(
-          name: 'Master of Computer Application(M.C.A.)', value: "value3"),
-      DropDownValueModel(name: 'Ph.D. Computer Science', value: "value4"),
-    ],
-    'science': [
-      DropDownValueModel(name: 'B.Sc(MPCs)', value: "value5"),
-      DropDownValueModel(name: 'B.Sc(MPC)', value: "value6"),
-      DropDownValueModel(name: 'B.Sc(ZBC)', value: "value7"),
-      DropDownValueModel(name: 'M.Sc Mathematics', value: "value8"),
-      DropDownValueModel(name: 'Ph.D. Mathematics', value: "value9"),
-      DropDownValueModel(name: 'Ph.D. Botany', value: "value10"),
-      DropDownValueModel(name: 'Ph.D. Zoology', value: "value11"),
-      DropDownValueModel(name: 'Ph.D. Chemistry', value: "value12"),
-      DropDownValueModel(name: 'Ph.D. Physics', value: "value13"),
-    ],
-    'mass': [
-      DropDownValueModel(
-          name: 'B.A. (Mass Communication & Journalism)', value: "value14"),
-      DropDownValueModel(
-          name: 'M.A. (Mass Communication & Journalism)', value: "value15"),
-      DropDownValueModel(
-          name: 'Ph.D. (Mass Communication & Journalism)', value: "value16"),
-    ],
-    'arabic': [
-      DropDownValueModel(
-          name: 'Certificate Course in Proficiency in Arabic',
-          value: "value17"),
-      DropDownValueModel(name: 'Diploma in Arabic', value: "value18"),
-      DropDownValueModel(
-          name: 'Advance Diploma in Professional Arabic', value: "value19"),
-      DropDownValueModel(name: 'M.A. Arabic', value: "value20"),
-      DropDownValueModel(name: 'Ph.D. Arabic', value: "value21"),
-    ],
-    'eng': [
-      DropDownValueModel(name: 'M.A. English', value: "value22"),
-      DropDownValueModel(name: 'Ph.D. English', value: "value23"),
-    ],
-    'hindi': [
-      DropDownValueModel(
-          name: 'PG Diploma in Functional Hindi and Translation',
-          value: "value24"),
-      DropDownValueModel(name: 'M.A. Hindi', value: "value25"),
-      DropDownValueModel(name: 'Ph.D. Hindi', value: "value26"),
-    ],
-    'persian': [
-      DropDownValueModel(name: 'Diploma in Persian', value: "value27"),
-      DropDownValueModel(name: 'M.A. Persian', value: "value28"),
-      DropDownValueModel(name: 'Ph.D. Persian', value: "value29"),
-    ],
-    'trans': [
-      DropDownValueModel(name: 'M.A. Translation Studies', value: "value30"),
-      DropDownValueModel(name: 'Ph.D. Translation Studies', value: "value31"),
-    ],
-    'urdu': [
-      DropDownValueModel(name: 'Certificate Course in Urdu', value: "value32"),
-      DropDownValueModel(
-          name: 'PG Diploma in Functional Urdu', value: "value33"),
-      DropDownValueModel(name: 'M.A. Urdu', value: "value34"),
-      DropDownValueModel(name: 'Ph.D. Urdu', value: "value35"),
-    ],
-    'edu': [
-      DropDownValueModel(
-          name: 'Diploma in Elementary Education', value: "value36"),
-      DropDownValueModel(name: 'Bachelor of Education(B.Ed)', value: "value37"),
-      DropDownValueModel(name: 'Master of Education(M.Ed)', value: "value38"),
-      DropDownValueModel(name: 'Ph.D. Education', value: "value39"),
-    ],
-    'comm & mang': [
-      DropDownValueModel(name: 'Bachelor of Commerce(B.Com)', value: "value40"),
-      DropDownValueModel(
-          name: 'Master of Business Administration(M.B.A.)', value: "value41"),
-      DropDownValueModel(name: 'Master of Commerce(M.Com)', value: "value42"),
-      DropDownValueModel(name: 'Ph.D. Commerce', value: "value43"),
-      DropDownValueModel(name: 'Ph.D. Management Studies', value: "value44"),
-    ],
-    'eco': [
-      DropDownValueModel(name: 'M.A. Economics', value: "value45"),
-      DropDownValueModel(name: 'Ph.D. Economics', value: "value46"),
-    ],
-    'his': [
-      DropDownValueModel(name: 'M.A. History', value: "value47"),
-      DropDownValueModel(name: 'Ph.D. History', value: "value48"),
-    ],
-    'poli sci': [
-      DropDownValueModel(name: 'M.A. Political Science', value: "value49"),
-      DropDownValueModel(name: 'Ph.D. Political Science', value: "value50"),
-    ],
-    'pub admin': [
-      DropDownValueModel(name: 'M.A. Public Administration', value: "value51"),
-      DropDownValueModel(name: 'Ph.D. Public Administration', value: "value52"),
-    ],
-    'social work': [
-      DropDownValueModel(name: 'Master of Social Work(MSW)', value: "value53"),
-      DropDownValueModel(name: 'Ph.D. Social Work', value: "value54"),
-    ],
-    'sociology': [
-      DropDownValueModel(name: 'M.A. Sociology', value: "value55"),
-      DropDownValueModel(name: 'Ph.D. Sociology', value: "value56"),
-    ],
-    'women edu': [
-      DropDownValueModel(name: 'M.A. Women Education', value: "value57"),
-      DropDownValueModel(name: 'Ph.D. Women Education', value: "value58"),
-    ],
-    'islamic stu': [
-      DropDownValueModel(name: 'M.A. Islamic Studies', value: "value59"),
-      DropDownValueModel(name: 'Ph.D. Islamic Studies', value: "value60"),
-    ],
-    'poly': [
-      DropDownValueModel(name: 'Diploma in Computer Science', value: "value61"),
-      DropDownValueModel(
-          name: 'Diploma in Civil Engineering', value: "value62"),
-      DropDownValueModel(name: 'Diploma in Electronic', value: "value63"),
-      DropDownValueModel(
-          name: 'Diploma in Information Technology', value: "value64"),
-    ],
-    'arts': [
-      DropDownValueModel(name: 'B.A. (Bachelor of Arts)', value: "value65"),
-    ],
-  };
   final UserDetails user = UserDetails();
 
-  String dropValue = 'btech';
+  String dropValue = '';
 
+  @override
+  void initState() {
+    dropValue = dept[0];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -230,18 +67,39 @@ class _RegistrationFormState extends State<RegistrationForm> {
                   child: Column(
                     children: [
                       DropdownButtonFormField(
+                          decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0)), //<-- SEE HERE
+                              borderSide:
+                                  BorderSide(color: Colors.white10, width: 2),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0)),
+                              //<-- SEE HERE
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white10,
+                          ),
+                          dropdownColor: Colors.white,
                           items: dept
-                              .map((e) => DropdownMenuItem<String>(
-                                    value: e['value'],
-                                    child: Text(e['name']!),
-                                  ))
+                              .map<DropdownMenuItem<String>>(
+                                  (String e) => DropdownMenuItem<String>(
+                                        value: e,
+                                        child: Text(e),
+                                      ))
                               .toList(),
                           // value: 'Select Dept',
-                          value: ['hello1'],
+                          value: dropValue.isNotEmpty ? dropValue : null,
                           borderRadius:
                               const BorderRadius.all(Radius.circular(20)),
                           onChanged: (val) {
-                            // user.dept = val.toString();
+                            setState(() {
+                              dropValue = val!;
+                            });
                           }),
                       TextFormField(
                         decoration: getDecoration('Full Name'),
@@ -301,7 +159,7 @@ class _RegistrationFormState extends State<RegistrationForm> {
                           }
                         },
                         dropDownItemCount: 30,
-                        dropDownList: dict[dropValue]!,
+                        dropDownList: Data.dict['btech']!,
                       ),
                       const SizedBox(
                         height: 10,
@@ -329,7 +187,9 @@ class _RegistrationFormState extends State<RegistrationForm> {
                         clearOption: true,
                         enableSearch: true,
                         onChanged: (val) {
-                          user.year = val.value;
+                          setState(() {
+                            user.year = val.value;
+                          });
                         },
                         validator: (value) {
                           if (value == null) {
---
+++ lib/static_data.dart
@@ -0,0 +1,169 @@
+import 'package:dropdown_textfield/dropdown_textfield.dart';
+
+class Data {
+  static const dict = {
+    'btech': [
+      DropDownValueModel(name: 'B.Tech Computer Science', value: "value1"),
+      DropDownValueModel(name: 'M.Tech Computer Science', value: "value2"),
+      DropDownValueModel(
+          name: 'Master of Computer Application(M.C.A.)', value: "value3"),
+      DropDownValueModel(name: 'Ph.D. Computer Science', value: "value4"),
+    ],
+    'science': [
+      DropDownValueModel(name: 'B.Sc(MPCs)', value: "value5"),
+      DropDownValueModel(name: 'B.Sc(MPC)', value: "value6"),
+      DropDownValueModel(name: 'B.Sc(ZBC)', value: "value7"),
+      DropDownValueModel(name: 'M.Sc Mathematics', value: "value8"),
+      DropDownValueModel(name: 'Ph.D. Mathematics', value: "value9"),
+      DropDownValueModel(name: 'Ph.D. Botany', value: "value10"),
+      DropDownValueModel(name: 'Ph.D. Zoology', value: "value11"),
+      DropDownValueModel(name: 'Ph.D. Chemistry', value: "value12"),
+      DropDownValueModel(name: 'Ph.D. Physics', value: "value13"),
+    ],
+    'mass': [
+      DropDownValueModel(
+          name: 'B.A. (Mass Communication & Journalism)', value: "value14"),
+      DropDownValueModel(
+          name: 'M.A. (Mass Communication & Journalism)', value: "value15"),
+      DropDownValueModel(
+          name: 'Ph.D. (Mass Communication & Journalism)', value: "value16"),
+    ],
+    'arabic': [
+      DropDownValueModel(
+          name: 'Certificate Course in Proficiency in Arabic',
+          value: "value17"),
+      DropDownValueModel(name: 'Diploma in Arabic', value: "value18"),
+      DropDownValueModel(
+          name: 'Advance Diploma in Professional Arabic', value: "value19"),
+      DropDownValueModel(name: 'M.A. Arabic', value: "value20"),
+      DropDownValueModel(name: 'Ph.D. Arabic', value: "value21"),
+    ],
+    'eng': [
+      DropDownValueModel(name: 'M.A. English', value: "value22"),
+      DropDownValueModel(name: 'Ph.D. English', value: "value23"),
+    ],
+    'hindi': [
+      DropDownValueModel(
+          name: 'PG Diploma in Functional Hindi and Translation',
+          value: "value24"),
+      DropDownValueModel(name: 'M.A. Hindi', value: "value25"),
+      DropDownValueModel(name: 'Ph.D. Hindi', value: "value26"),
+    ],
+    'persian': [
+      DropDownValueModel(name: 'Diploma in Persian', value: "value27"),
+      DropDownValueModel(name: 'M.A. Persian', value: "value28"),
+      DropDownValueModel(name: 'Ph.D. Persian', value: "value29"),
+    ],
+    'trans': [
+      DropDownValueModel(name: 'M.A. Translation Studies', value: "value30"),
+      DropDownValueModel(name: 'Ph.D. Translation Studies', value: "value31"),
+    ],
+    'urdu': [
+      DropDownValueModel(name: 'Certificate Course in Urdu', value: "value32"),
+      DropDownValueModel(
+          name: 'PG Diploma in Functional Urdu', value: "value33"),
+      DropDownValueModel(name: 'M.A. Urdu', value: "value34"),
+      DropDownValueModel(name: 'Ph.D. Urdu', value: "value35"),
+    ],
+    'edu': [
+      DropDownValueModel(
+          name: 'Diploma in Elementary Education', value: "value36"),
+      DropDownValueModel(name: 'Bachelor of Education(B.Ed)', value: "value37"),
+      DropDownValueModel(name: 'Master of Education(M.Ed)', value: "value38"),
+      DropDownValueModel(name: 'Ph.D. Education', value: "value39"),
+    ],
+    'comm & mang': [
+      DropDownValueModel(name: 'Bachelor of Commerce(B.Com)', value: "value40"),
+      DropDownValueModel(
+          name: 'Master of Business Administration(M.B.A.)', value: "value41"),
+      DropDownValueModel(name: 'Master of Commerce(M.Com)', value: "value42"),
+      DropDownValueModel(name: 'Ph.D. Commerce', value: "value43"),
+      DropDownValueModel(name: 'Ph.D. Management Studies', value: "value44"),
+    ],
+    'eco': [
+      DropDownValueModel(name: 'M.A. Economics', value: "value45"),
+      DropDownValueModel(name: 'Ph.D. Economics', value: "value46"),
+    ],
+    'his': [
+      DropDownValueModel(name: 'M.A. History', value: "value47"),
+      DropDownValueModel(name: 'Ph.D. History', value: "value48"),
+    ],
+    'poli sci': [
+      DropDownValueModel(name: 'M.A. Political Science', value: "value49"),
+      DropDownValueModel(name: 'Ph.D. Political Science', value: "value50"),
+    ],
+    'pub admin': [
+      DropDownValueModel(name: 'M.A. Public Administration', value: "value51"),
+      DropDownValueModel(name: 'Ph.D. Public Administration', value: "value52"),
+    ],
+    'social work': [
+      DropDownValueModel(name: 'Master of Social Work(MSW)', value: "value53"),
+      DropDownValueModel(name: 'Ph.D. Social Work', value: "value54"),
+    ],
+    'sociology': [
+      DropDownValueModel(name: 'M.A. Sociology', value: "value55"),
+      DropDownValueModel(name: 'Ph.D. Sociology', value: "value56"),
+    ],
+    'women edu': [
+      DropDownValueModel(name: 'M.A. Women Education', value: "value57"),
+      DropDownValueModel(name: 'Ph.D. Women Education', value: "value58"),
+    ],
+    'islamic stu': [
+      DropDownValueModel(name: 'M.A. Islamic Studies', value: "value59"),
+      DropDownValueModel(name: 'Ph.D. Islamic Studies', value: "value60"),
+    ],
+    'poly': [
+      DropDownValueModel(name: 'Diploma in Computer Science', value: "value61"),
+      DropDownValueModel(
+          name: 'Diploma in Civil Engineering', value: "value62"),
+      DropDownValueModel(name: 'Diploma in Electronic', value: "value63"),
+      DropDownValueModel(
+          name: 'Diploma in Information Technology', value: "value64"),
+    ],
+    'arts': [
+      DropDownValueModel(name: 'B.A. (Bachelor of Arts)', value: "value65"),
+    ],
+  };
+  final dept_name =  const [
+    DropDownValueModel(name: 'Select Dept', value: "Select Dept"),
+    DropDownValueModel(name: 'Department of Technology', value: "btech"),
+    DropDownValueModel(
+      name: 'Department of Science',
+      value: "science",
+    ),
+    DropDownValueModel(
+        name: 'Department of Mass Communication & Journalism', value: "mass"),
+    DropDownValueModel(
+      name: 'Polytechnic',
+      value: "poly",
+    ),
+    DropDownValueModel(
+        name: 'Department of Education & Training', value: "edu"),
+    DropDownValueModel(
+        name: 'Department of Commerce & Business Management',
+        value: "comm & mang"),
+    DropDownValueModel(
+        name: 'Department of Arts & Social Science', value: "arts"),
+    DropDownValueModel(name: 'Department of Arabic', value: "arabic"),
+    DropDownValueModel(name: 'Department of English', value: "eng"),
+    DropDownValueModel(name: 'Department of Hindi', value: "hindi"),
+    DropDownValueModel(
+        name: 'Department of Persian and Central Asian Studies',
+        value: "persian"),
+    DropDownValueModel(
+        name: 'Department of Translation Studies', value: "trans"),
+    DropDownValueModel(name: 'Department of Urdu', value: "urdu"),
+    DropDownValueModel(name: 'Department of Economics', value: "eco"),
+    DropDownValueModel(name: 'Department of History', value: "his"),
+    DropDownValueModel(
+        name: 'Department of Political Science', value: "poli sci"),
+    DropDownValueModel(
+        name: 'Department of Public Administration', value: "pub admin"),
+    DropDownValueModel(name: 'Department of Social Work', value: "social"),
+    DropDownValueModel(name: 'Department of Sociology', value: "sociology"),
+    DropDownValueModel(
+        name: 'Department of Women Education', value: "women edu"),
+    DropDownValueModel(
+        name: 'Department of Islamic Studies', value: "islamic stu"),
+  ];
+}
